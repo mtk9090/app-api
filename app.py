a1d3de54503c406dd79fcbff01e8ad3acb2c4019
msnpy <53246704+msnpy@users.noreply.github.com>
2021-06-08 14:40:27 -0300

    arquivos versao2

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import requests
+
+
+app = Flask(__name__)
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    search = request.form.get('nome_digitado')
+    print(search)
+    try:
+        url = f'http://ip-api.com/json/{search}'
+        req = requests.get(url)
+        resp = req.json()
+
+    except:
+        return render_template('onfline.html')
+
+
+    else:
+        return render_template('ip.html', resp=resp)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
