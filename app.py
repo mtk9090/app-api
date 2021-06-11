55382b1efde68996a45b7010c0bd2e4a09b8287b
mtk9090 <73513297+mtk9090@users.noreply.github.com>
2021-06-11 20:49:44 -0300

    error 404

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def index():
         return render_template('ip.html', resp=resp)
 
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     import os
 
